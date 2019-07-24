ef0f26f573e9877d34637f9cbc334556e785d6fd
Jamie Vullo <jvullo@protonmail.com>
2019-07-24 14:40:22 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,10 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Base[5.1]
   
   def change 
-    
+    add_column do |t|
+      t.integer :grade
+      t.string :birthdate 
+    end
   end
 
   
